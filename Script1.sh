78e1a98c6394da63df8960bad972c1b5fc605275
Amit <amit24sapkal@gmail.com>
2026-02-19 20:30:43 +0530

    Amit's line deleted

--- Script1.sh
+++ Script1.sh
@@ -4,7 +4,6 @@ echo "We are learning Branching"
 echo "I am learning Devops"
 echo "Adding new line"
 echo "I am from Devops and cloud team"
-echo "This is Amit's new line"
 echo "This is Rupali's new line"
 
 
