9b2be62b3b1241eb43d4e4a917209e75625a48bf
root <root@ip-172-31-39-206.ap-south-1.compute.internal>
2026-02-20 09:04:30 +0000

    Code is updated from EC2

--- Script1.sh
+++ Script1.sh
@@ -5,7 +5,7 @@ echo "I am learning Devops"
 echo "Adding new line"
 echo "I am from Devops and cloud team"
 echo "This is Rupali's new line"
-echo "This is 2nd new line"
+echo "This line is from EC2"
 
 
 
