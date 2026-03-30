c03030d05bae2509ec42a7b34822b3c2febe46ce
Amit <amit24sapkal@gmail.com>
2026-03-30 21:17:34 +0530

    This is Script1 update-2

--- Script1.sh
+++ Script1.sh
@@ -9,7 +9,7 @@ echo "This line is from EC2"
 echo " This line from Rupali"
 echo "this is Amit"
 echo "this is Shreya"
-echo "we are happy Family Forever"
+echo "we are happy Family Ever & Forever"
 
 
 
